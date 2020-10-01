637dab2baaec0a53b8453b7d41957bc59008ad8a
Crowdbotics <team@crowdbotics.com>
2020-10-01 17:11:28 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/mobole_89_dev_12314/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mobole_89_dev_12314;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "mobole_89_dev_12314";
+    }
+}
---
+++ backend/mobole_89_dev_12314/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mobole_89_dev_12314 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mobole_89_dev_12314.settings')
+
+application = get_wsgi_application()
